cacf57d135bdaf10cba7c032fb893e9fa826c3e8
Robert <bert210@gmail.com>
2017-10-17 15:05:03 -0400

    Fetch data with a proxy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 const NFL_TEAMS = ["ARI","ATL","BAL","BUF","CAR","CHI","CIN","CLE","DAL","DEN","DET","GB","HOU","IND","JAC","KC","LA","MIA","MIN","NE","NO","NYG","NYJ","OAK","PHI","PIT","SD","SEA","SF","TB","TEN","WAS"]
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const getRosterURL = (team) => {
+  return `http://api.suredbits.com/nfl/v0/team/${team}/roster`
+  // return 'http://api.suredbits.com/nfl/v0/info/'
+}
 
 class App extends Component {
+  componentDidMount() {
+    fetch(proxyUrl + getRosterURL(NFL_TEAMS[0]))
+      .then(d => d.json())
+      .then(d => console.log(d))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        // <header className="App-header">
+        //   <img src={logo} className="App-logo" alt="logo" />
+        //   <h1 className="App-title">Welcome to React</h1>
+        // </header>
+        // <p className="App-intro">
+        //   To get started, edit <code>src/App.js</code> and save to reload.
+        // </p>
       </div>
     );
   }
