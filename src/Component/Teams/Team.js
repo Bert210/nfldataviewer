f71efa1b0d0e71903c759c462c3fbb889055b86b
Robert <bert210@gmail.com>
2017-10-20 06:46:11 -0400

    Fixed the logo on the team page

--- src/Component/Teams/Team.js
+++ src/Component/Teams/Team.js
@@ -10,13 +10,28 @@ import React from 'react'
 import TeamsData from '../../teamColors.json'
 
 class Team extends React.Component {
+	constructor(props) {
+		super(props)
+		let teams = TeamsData.map((val, index) => {
+			val.logoName = this.convertNameToId(val.name)
+			return val
+		})
+		this.state = {
+			teams
+		}
+	}
+
+	convertNameToId = (name) => {
+		return name.replace(/\s+/g, '-').toLowerCase()
+	}
+
 	render() {
 		let selectedTeam = TeamsData.find((val) => {return val.id === this.props.match.params.teamName})
 		console.log(selectedTeam)
 		return (
 			<div>
-				<img src={`static/logos/${selectedTeam.logoName}.svg`} alt="Team Logo"></img>
-				<div>{selectedTeam.name}</div></div>)
+				<img src={`../static/logos/${selectedTeam.logoName}.svg`} alt={`${selectedTeam.name}'s Logo`} />
+				<div style={{"color": `#${selectedTeam.colors.hex[0]}`}}>{selectedTeam.name}</div></div>)
 	}
 }
 
