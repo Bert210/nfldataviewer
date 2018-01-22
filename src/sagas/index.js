3a423dd9fb758f498c10a5ec17f8436a8868f3e9
Robert <bert210@gmail.com>
2018-01-22 07:33:57 -0500

    Refactored Roster to use redux

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
 import { fork } from 'redux-saga/effects';
 import scheduleContainerSagas from '../containers/ScheduleContainer/sagas';
-
+import rosterContainerSagas from '../containers/RosterContainer/sagas';
 // export default [
 //     yield fork(...scheduleContainerSagas),
 // ];r
@@ -8,5 +8,6 @@ import scheduleContainerSagas from '../containers/ScheduleContainer/sagas';
 export default function* rootSaga() {
     yield [
         fork(scheduleContainerSagas),
+        fork(rosterContainerSagas),
     ]
 }
